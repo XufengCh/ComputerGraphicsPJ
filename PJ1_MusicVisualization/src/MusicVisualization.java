1c971abcced7afd4592ff37c94051dd896022d2d
XufengCh <xfch293@gmail.com>
2019-11-26 22:10:43 +0800

    finish PJ1

--- PJ1_MusicVisualization/src/MusicVisualization.java
+++ PJ1_MusicVisualization/src/MusicVisualization.java
@@ -1,42 +1,47 @@
-import ddf.minim.analysis.*;
-import ddf.minim.Minim;
-import ddf.minim.AudioPlayer;
 import processing.core.PApplet;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+import ddf.minim.analysis.FFT;
 
 import java.io.File;
 
 public class MusicVisualization extends PApplet{
     private static String fileName;
-    private static boolean isRunning = false;
+    private static boolean isLoaded = false;
 
     private Minim minim;
     private AudioPlayer player;
     private FFT fft;
 
-    private int circleX;
-    private int circleY;
+    @Override
+    public void settings() {
+        size(512, 400, P3D);
+    }
 
-    /**
-     * select the file to open
-     * @param file the file to open
-     */
-    public void fileSelect(File file){
+    @Override
+    public void setup() {
+        background(0x000000);
+        selectInput("请选择输入的音频文件： ", "loadFile");
+    }
+
+    public void loadFile(File file){
         if(file == null){
             println("No file selected. Exit. ");
-            stop();
+            exit();
         }else{
-            String filePath = file.getAbsolutePath();
             fileName = file.getName();
-            isRunning = true;
-            println("User selected: " + filePath);
-            // load file
+            println("User Select: " + file.getAbsolutePath());
+
+            //load file
             minim = new Minim(this);
-            player = minim.loadFile(filePath);
+            player = minim.loadFile(file.getAbsolutePath());
             fft = new FFT(player.bufferSize(), player.sampleRate());
-            player.play();
             println(fileName + " is playing...");
+            isLoaded = true;
+            player.play();
         }
     }
+
     @Override
     public void stop(){
         if(player != null){
@@ -47,30 +52,26 @@ public class MusicVisualization extends PApplet{
     }
 
     @Override
-    public void settings(){
-        size(512, 512);
-    }
+    public void draw() {
+        if(!isLoaded) return;
 
-    @Override
-    public void setup(){
-        size(512, 512);
         background(0x000000);
-        selectInput("Select a file: ", "fileSelect");
-
-        circleX = width/2;
-        circleY = 145;
+        displayFileName();
+        drawWaveform();
+        drawFrequecySpectrum();
     }
 
-    private void displayFileName(){
-        /*PFont font = createFont("Arial",16,true);
-        textFont(font, 16);*/
-        //        fill(0xffffff);
-        //        textAlign(CENTER, CENTER);
-        //        //fill(0x777777);
-        text(fileName, 256, height - 200);
+    public void displayFileName(){
+        fill(119, 119, 119);
+        textAlign(CENTER, CENTER);
+        textMode(SHAPE);
+        textSize(13);
+        text(fileName, width/2, height - 100);
     }
 
-    private void drawWaveform(){
+    public void drawWaveform(){
+        int circleX = width/2;
+        int circleY = 150;
         float a = 0;
         int slices = player.bufferSize();
         float angle = (2 * PI) / slices;
@@ -82,43 +83,27 @@ public class MusicVisualization extends PApplet{
             float left_y2 = circleY + sin(a + angle) * (50 * player.left.get(i + 1) + 75);
             // the right sound channel
             float right_x1 = circleX + cos(a) * (50 * player.right.get(i) + 75);
-            float right_y1 = circleX + sin(a) * (50 * player.right.get(i) + 75);
+            float right_y1 = circleY + sin(a) * (50 * player.right.get(i) + 75);
             float right_x2 = circleX + cos(a + angle) * (50 * player.right.get(i + 1) + 75);
-            float right_y2 = circleX + sin(a + angle) * (50 * player.right.get(i + 1) + 75);
+            float right_y2 = circleY + sin(a + angle) * (50 * player.right.get(i + 1) + 75);
             a += angle;
             // draw
-            stroke(0xffee99, (float) 0.5);
+            stroke(255, 238, 153, 127);
             line(left_x1, left_y1, left_x2, left_y2);
             line(right_x1, right_y1, right_x2, right_y2);
         }
+        noStroke();
     }
 
-    private void drawFrequencySpectrum(){
+    public void drawFrequecySpectrum(){
         fft.forward(player.mix);
 
         noStroke();
-        fill(0xee7700, (float) 0.3);
+        fill(238, 119, 0, 190);
         for(int i = 0; i < width/4; i++){
             float b = fft.getBand(i);
             rect(i * 4, height - b, 4, b);
         }
-    }
-
-    @Override
-    public void draw(){
-        if(!isRunning) return;
-
-        background(0x000000);
-        // fileName
-        displayFileName();
-        /*// waveform
-        fill(0x9999ff, (float) 0.7);
-        ellipse(circleX, circleY, 25, 25);
-        fill(0xffee99, (float) 0.7);
-        ellipse(circleX, circleY, 20, 20);
-        drawWaveform();
-        // frequency spectrum
-        drawFrequencySpectrum();*/
     }
 
     public static void main(String[] args){
